e22d51532461ad3df64384b805b6fedad347acd4
Ramiro Santiago Bautista <santiagobautr@queens.edu>
2020-08-07 12:22:35 -0400

    The JS has been changed

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+// const navSlide = () => {
+//     const burger = document.querySelector('.burger');
+//     const nav = document.querySelector('.nav-links');
+//     const navLinks = document.querySelectorAll('.nav-links li');
 
-    burger.addEventListener('click', () => {
-        // Toggle Nav
-        nav.classList.toggle('nav-active');
+//     burger.addEventListener('click', () => {
+//         // Toggle Nav
+//         nav.classList.toggle('nav-active');
     
-    // Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else{
-                link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s';
-            }
-        });
-        //Burger Animation
-        burger.classList.toggle('toggle');
-    });
-}
+//     // Animate Links
+//         navLinks.forEach((link, index) => {
+//             if (link.style.animation) {
+//                 link.style.animation = '';
+//             } else{
+//                 link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s';
+//             }
+//         });
+//         //Burger Animation
+//         burger.classList.toggle('toggle');
+//     });
+// }
 
-navSlide();
+// navSlide();
 
-function val() {
-    var name = document.getElementById('name');
-    var email = document.getElementById('email');
-    var pNumber = document.getElementById('phone');
+// function val() {
+//     var name = document.getElementById('name');
+//     var email = document.getElementById('email');
+//     var pNumber = document.getElementById('phone');
 
-    if (name.value.trim() == '' || email.value.trim() == '' || phone.value.trim() == '') {
-        alert('Fill out the empty fields.');
-        return false;
-    }
-    else{
-        true;
-    }
-}
+//     if (name.value.trim() == '' || email.value.trim() == '' || phone.value.trim() == '') {
+//         alert('Fill out the empty fields.');
+//         return false;
+//     }
+//     else{
+//         true;
+//     }
+// }
 
-var slideIndex = 0;
-carousel();
+// var slideIndex = 0;
+// carousel();
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none"; 
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1} 
-  x[slideIndex-1].style.display = "block"; 
-  setTimeout(carousel, 1000); 
-}
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none"; 
+//   }
+//   slideIndex++;
+//   if (slideIndex > x.length) {slideIndex = 1} 
+//   x[slideIndex-1].style.display = "block"; 
+//   setTimeout(carousel, 1000); 
+// }
